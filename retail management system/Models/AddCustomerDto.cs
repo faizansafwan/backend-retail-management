4f82c7eeeaffb303dd6182c4ac042c8e66e72678
faizansafwan <faizansafwan075@gmail.com>
2025-03-28 11:02:32 +0530

    controller for customer

---
+++ retail management system/Models/AddCustomerDto.cs
@@ -0,0 +1,13 @@
+﻿namespace retail_management_system.Models
+{
+    public class AddCustomerDto
+    {
+        public required string CustomerName { get; set; }
+        public string? Email { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string Address { get; set; }
+        public long CreditLimit { get; set; }
+        public long OpeningBalance { get; set; }
+
+    }
+}
