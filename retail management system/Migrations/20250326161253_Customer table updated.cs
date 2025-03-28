81dfea020432a23032469514838bde74055663e5
faizansafwan <faizansafwan075@gmail.com>
2025-03-28 11:10:16 +0530

    customer table updated

---
+++ retail management system/Migrations/20250326161253_Customer table updated.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace retail_management_system.Migrations
+{
+    /// <inheritdoc />
+    public partial class Customertableupdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerName",
+                table: "Customers",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_CustomerName",
+                table: "Customers",
+                column: "CustomerName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_CustomerName",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerName",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+        }
+    }
+}
