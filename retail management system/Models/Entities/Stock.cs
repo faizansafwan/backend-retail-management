f45caae217a461d89a8e0057973d2bf048e64b36
faizansafwan <faizansafwan075@gmail.com>
2025-03-30 14:50:11 +0530

    Stock model and controller updated

--- retail management system/Models/Entities/Stock.cs
+++ retail management system/Models/Entities/Stock.cs
@@ -1,13 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace retail_management_system.Models.Entities
 {
     public class Stock
     {
         public int Id { get; set; }
-       
+
         public required int ProductId { get; set; }
-        [ForeignKey("ProductId")]
+
+        [ForeignKey(nameof(ProductId))] // Reference Product.ProductId
         public Product Product { get; set; } = null!;
 
         [Column(TypeName = "decimal(18,2)")]
@@ -15,7 +17,7 @@ namespace retail_management_system.Models.Entities
 
         [Column(TypeName = "decimal(18,2)")]
         public required decimal SellingPrice { get; set; }
-        public required int CurrentStock { get; set; }
+        public int CurrentStock { get; set; } = 0;
         public required int StockAdjustment {  get; set; }
         
         public int NewStock { get; set; }
@@ -23,7 +25,11 @@ namespace retail_management_system.Models.Entities
         [Column(TypeName = "decimal(18,2)")]
         public decimal Total {  get; set; }
 
-        
+        public int ShopId { get; set; } // Foreign key
+        [ForeignKey("ShopId")]
+        public Shop? Shop { get; set; } // Navigation Property
+
+
 
         public List<InvoiceStock> InvoiceStocks { get; set; } = new();
 
