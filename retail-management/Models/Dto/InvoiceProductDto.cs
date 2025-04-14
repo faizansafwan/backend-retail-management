7b708f411d3d74dbd9f112fe181e635c9251aeee
faizansafwan <faizansafwan075@gmail.com>
2025-04-14 20:53:54 +0530

    dto created for invoice product and stock

---
+++ retail-management/Models/Dto/InvoiceProductDto.cs
@@ -0,0 +1,14 @@
+﻿namespace retail_management.Models.Dto
+{
+    public class InvoiceProductDto
+    {
+        public required int ProductId { get; set; }
+        public required int Quantity { get; set; }
+        public required decimal SellingPrice { get; set; }
+        public decimal Discount { get; set; }
+        public required decimal SubTotal { get; set; }
+        public required decimal Total { get; set; }
+        public decimal Paid { get; set; }
+        public required decimal Balance { get; set; }
+    }
+}
---
+++ retail-management/Models/Dto/InvoiceStockDto.cs
@@ -0,0 +1,8 @@
+﻿namespace retail_management.Models.Dto
+{
+    public class InvoiceStockDto
+    {
+        public required int StockId { get; set; }
+        public required int Quantity { get; set; }
+    }
+}
