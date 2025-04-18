7bfff1f77a63f5900611ad80915f32a0c4f51572
faizansafwan <faizansafwan075@gmail.com>
2025-04-19 00:18:01 +0530

    dto for join the product and stock table

---
+++ retail-management/Models/Dto/ProductWithStockDto.cs
@@ -0,0 +1,15 @@
+﻿namespace retail_management.Models.Dto
+{
+    public class ProductWithStockDto
+    {
+            public int ProductId { get; set; }
+            public string ProductName { get; set; } = string.Empty;
+            public string? Category { get; set; }
+            public string? Supplier { get; set; }
+            public int TotalStock { get; set; }
+            public decimal CostPrice { get; set; } 
+            public decimal SellingPrice { get; set; }
+            public decimal Total { get; set; }
+
+    }
+}
