80890c732209c780aaf2b9e19b2a97b17b574173
faizansafwan <faizansafwan075@gmail.com>
2025-04-22 23:46:31 +0530

    added column - total,balance and paid

--- retail-management/Models/Entities/Invoice.cs
+++ retail-management/Models/Entities/Invoice.cs
@@ -5,14 +5,23 @@ namespace retail_management.Models.Entities
     public class Invoice
     {
         public int Id { get; set; }
-        public required int CustomerId { get; set; }
+        public int CustomerId { get; set; }
 
         [ForeignKey("CustomerId")]
         public Customer Customer { get; set; } = null!;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Total { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Paid { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Balance { get; set; }
+
         public DateTime InvoiceDate { get; set; } = DateTime.UtcNow;
 
         public List<InvoiceProduct> InvoiceProducts { get; set; } = new();
         public List<InvoiceStock> InvoiceStocks { get; set; } = new();
-
     }
 }
