b2631a8ff74362dec5b2201730fd1b80ec8c417c
faizansafwan <faizansafwan075@gmail.com>
2025-04-23 17:33:42 +0530

    cache

---
+++ retail-management/Migrations/20250423060625_Quantity deleted.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace retail_management.Migrations
+{
+    /// <inheritdoc />
+    public partial class Quantitydeleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "InvoiceStocks");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "InvoiceStocks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
